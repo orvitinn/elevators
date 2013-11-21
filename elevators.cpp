2d30fd3ba54e4557934de3260f2c9994d7cfe889
Matthías Ásgeirsson <matthias.asgeirsson@gmail.com>
2013-11-21 13:46:40 +0000

    Read names from file in all nodes, assign to node according to rank

--- elevators.cpp
+++ elevators.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <vector>
 #include <string>
+#include <sstream>
 
 #include "mpi.h"
 
@@ -18,7 +19,11 @@ enum State { New, Working, Moving, Gone, Reading, Writing };
 
 const int RUNTIME_SECONDS = 20.0;
 char output_file_name[] = "/home/maa33/code/elevators/data.out";
+char input_file_name[] = "/home/maa33/code/elevators/names.txt";
 
+
+// all nodes store all the names in the same order for logging. First name in vector is person with id=0
+vector<std::string> names;
 
 double START;
 
@@ -94,9 +99,8 @@ void simulateElevator(int rank, MPI_Comm& lyftuhopur) {
     bool run_simulation = true;
     int count = 0;
     
-    MPI_File fh;
     char buffer[4];
-    
+    MPI_File fh;
     int rc = MPI_File_open(MPI_COMM_WORLD, output_file_name, MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
     // int rc = MPI_File_open(lyftuhopur, file_name, MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
     // int rc = MPI_File_open( MPI_COMM_WORLD, file_name, MPI_MODE_CREATE | MPI_MODE_RDWR | MPI_MODE_SEQUENTIAL, MPI_INFO_NULL, &out_file);
@@ -173,41 +177,18 @@ void simulateFloor(int rank, MPI_Comm& haedahopur) {
     MPI_File fh, fh2;
     MPI_Info info;
     MPI_Status status;
-    
-#if 1
-    char input_file_name[] = "/home/maa33/code/elevators/names.txt";
-    char native[] = "native";
-    int rc = MPI_File_open( haedahopur, input_file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-    MPI_File_set_view(fh, rank*sizeof(char)*15, MPI_CHAR, MPI_CHAR, native, MPI_INFO_NULL);
-
-
-    for (int i=0; i<2; i++)
+ 
+    std::stringstream ss;
+    ss << "floor: " << rank;
+    for (int i=rank; i<names.size(); i+=3)
     {
-        MPI_File_read(fh, buf, 14, MPI_CHAR, &status);
-        cout << rank << " read name from file: " << buf << endl;
+        ss << "[" << i << "] " << names[i];
+        persons.push_back(Person(i));
     }
-    
-    rc = MPI_File_close(&fh);
-#endif
+    cout << ss.str() << endl;
 
-    rc = MPI_File_open(MPI_COMM_WORLD, output_file_name, MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh2);
-    
-    
-    // lesa nöfn úr skrá með mpi_io - en fyrst - setja inn einhver gildi
-    if (rank == 0) {
-        persons.push_back(Person(1));
-        persons.push_back(Person(2));
-    }
-    else if (rank == 1)
-    {
-        persons.push_back(Person(3));
-        persons.push_back(Person(4));
-    }
-    else if (rank == 2)
-    {
-        persons.push_back(Person(5));
-        persons.push_back(Person(6));
-    }
+    int rc = MPI_File_open(MPI_COMM_WORLD, output_file_name, MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh2);
+        
     for (Person& p: persons)
         p.start_work();
     
@@ -272,11 +253,19 @@ void simulateFloor(int rank, MPI_Comm& haedahopur) {
 }
 
 
+void split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while(std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+
 int main(int argc, char* argv[]) {
     MPI_Status status;
     // MPI_Comm comm,scomm;
     int rank, size, color, errs=0;
-   
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
@@ -286,6 +275,28 @@ int main(int argc, char* argv[]) {
 
     START = MPI_Wtime();
 
+    // read the whole file in all nodes, then assign every n-th person to each of the n floors
+    char native[] = "native";
+    MPI_File fh;
+    int rc = MPI_File_open( MPI_COMM_WORLD, input_file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+    MPI_Offset  filesize;
+    MPI_File_get_size(fh, &filesize);
+    // MPI_File_set_view(fh, rank*sizeof(char)*15, MPI_CHAR, MPI_CHAR, native, MPI_INFO_NULL);
+    vector<char> name_buffer(static_cast<int>(filesize));
+    MPI_File_read(fh, &name_buffer[0], static_cast<int>(filesize), MPI_CHAR, &status);
+    rc = MPI_File_close(&fh);
+
+    std::string name_string_buffer(name_buffer.begin(), name_buffer.end());
+
+#if 1
+    if (rank == 0)
+    {
+        cout << "Las:\n" << name_string_buffer << endl;
+    }
+#endif
+    
+    // the names are in name_buffer, split it on newlines, then strip whitespaces from the end. Convert each to a string and push_back the strings to the names vector
+    split(name_string_buffer, '\n', names);
     
     // búum til com fyrir lyfturnar.
     MPI_Group lyftuhopur, haedahopur, allir;
